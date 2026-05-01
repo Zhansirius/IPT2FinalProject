904de225da3493522b92d2c634acbda9c6f0f2fb
Zhansirius <sergeyevzhansir@gmail.com>
2026-05-01 13:30:00 +0500

    Zhansir's work

---
+++ assets/tsx_tilesets/tilemap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="tilemap" tilewidth="24" tileheight="24" spacing="1" margin="1" tilecount="90" columns="15">
+ <image source="../tilesets/kenney/Tilemap/tilemap.png" width="379" height="170"/>
+</tileset>
