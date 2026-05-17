73a32a960f3c173083f02e14f37395ed968c2e71
Zhansirius <sergeyevzhansir@gmail.com>
2026-05-17 23:50:20 +0500

    Fixes

--- GameView.py
+++ GameView.py
@@ -46,7 +46,7 @@ class GameView(arcade.View):
         self.end_of_map = 0
 
         # Level number to load
-        self.level = 10
+        self.level = 1
 
         # Variable to hold our texture for our player
         self.player_texture = None
